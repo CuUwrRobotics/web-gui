a670598825b0cd169b9060df8e4a91d1a0f03514
Nicholas Steele <nichlock@gmail.com>
2020-09-01 20:37:51 -0400

    Moving docker to submodule

--- Docker/scripts-local/append_bashrc.sh
+++
@@ -1,9 +0,0 @@
-echo Starting bashrc appends
-# Colorize prompt and include container ID in it
-echo "PS1='\[\033[0m\]\u@\[\033[1;32m\]\h\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]\\$ '" >> /root/.bashrc
-
-# ROS setups
-echo "source /opt/ros/melodic/setup.sh" >> /root/.bashrc
-echo "source $HOME/catkin_ws/devel/setup.sh" >> /root/.bashrc
-
-echo Finished bashrc appends
